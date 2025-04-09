ad2ee28394e30c32e4b465197b74266309f7c9dd
sdbaner <doyel4@gmail.com>
2025-04-09 22:22:29 +0200

    ecr

---
+++ iac/stack/ecr/backend.tf
@@ -0,0 +1,6 @@
+backend "s3" {
+    bucket = "cozero-terraform-state-bucket"
+    key    = "shared/ecr/terraform.tfstate"
+    dynamodb_table = "terraform-lock-table"
+    region = "eu-central-1"
+}
--- iac/stack/ecr/state.tf
+++
@@ -1,9 +0,0 @@
-# state.tf
-terraform {
-  backend "s3" {
-    bucket = "" 
-    key    = ""
-    region = ""
-    profile= ""
-  }
-}
