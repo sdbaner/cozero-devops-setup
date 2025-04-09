7282bde72e6ef31c59aa29d95807969cd76ec8a8
Doyel Banerjee <50516377+sdbaner@users.noreply.github.com>
2025-04-09 20:56:06 +0200

    Update vpc.backend.tf

--- iac/vpc.backend.tf
+++ iac/vpc.backend.tf
@@ -2,9 +2,9 @@ terraform {
   backend "s3" {
     bucket = "terraform-state-bucket-cozero"
     key    = "shared/vpc/terraform.tfstate"
+    dynamodb_table = "terraform-lock"
     region = "eu-central-1"
     
     # enable state locking with dynamoDB
-    # intentionally disabled to not get charged
   }
 }
