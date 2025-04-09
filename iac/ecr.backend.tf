4a9727bb2016042ada6ca742bffabe51e30b1814
Doyel Banerjee <50516377+sdbaner@users.noreply.github.com>
2025-04-09 21:36:23 +0200

    Update ecr.backend.tf

--- iac/ecr.backend.tf
+++ iac/ecr.backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-bucket-cozero"
+    bucket = "cozero-terraform-state-bucket"
     key    = "shared/ecr/terraform.tfstate"
-    dynamodb_table = "terraform-lock"
+    dynamodb_table = "terraform-lock-table"
     region = "eu-central-1"
     
     # enable state locking with dynamoDB
