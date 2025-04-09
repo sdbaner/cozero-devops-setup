e0bc495aceebc3593c0cfece816ae373f5e38617
Doyel Banerjee <50516377+sdbaner@users.noreply.github.com>
2025-04-09 13:52:33 +0200

    Update variables.tf

--- iac/stack/ecr/variables.tf
+++ iac/stack/ecr/variables.tf
@@ -2,3 +2,8 @@ variable "ecr_name" {
   description = "ECR private repository"
   type        = string
 }
+
+variable "environment" {
+  description = "Environment tag (e.g. dev, qa, prod)"
+  type        = string
+}
