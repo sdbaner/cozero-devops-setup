59d0c3c9541f6e7bcc81ab3029f82aae92e19c69
Doyel Banerjee <50516377+sdbaner@users.noreply.github.com>
2025-04-09 22:17:50 +0200

    Update ecr.backend.tf

--- iac/ecr.backend.tf
+++ iac/ecr.backend.tf
@@ -1,11 +1,7 @@
-terraform {
-  backend "s3" {
+backend "s3" {
     bucket = "cozero-terraform-state-bucket"
     key    = "shared/ecr/terraform.tfstate"
     dynamodb_table = "terraform-lock-table"
     region = "eu-central-1"
-    
-    # enable state locking with dynamoDB
-
-  }
 }
+
