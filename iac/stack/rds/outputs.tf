cebbcdaee77af1cdd3d8bef0f66b4cb3f9ceab87
Doyel Banerjee <50516377+sdbaner@users.noreply.github.com>
2025-04-10 09:30:14 +0200

    Update outputs.tf

--- iac/stack/rds/outputs.tf
+++ iac/stack/rds/outputs.tf
@@ -1,4 +1,23 @@
+output "rds_hostname" {
+  description = "RDS instance hostname"
+  value       = aws_db_instance.postgresql.address
+  sensitive   = true
+}
+
+output "rds_port" {
+  description = "RDS instance port"
+  value       = aws_db_instance.postgresql.port
+  sensitive   = true
+}
+
+output "rds_username" {
+  description = "RDS instance root username"
+  value       = aws_db_instance.postgresql.username
+  sensitive   = true
+}
+
+
 output "rds_endpoint" {
   description = "The endpoint of the RDS instance"
-  value = aws_db_instance.postgres.endpoint
+  value = aws_db_instance.postgresql.endpoint
 }
