24f8f54a7e727cf312c4d6d714a22d4f11aad57b
Doyel Banerjee <50516377+sdbaner@users.noreply.github.com>
2025-04-10 19:14:08 +0200

    Create alb.tf

---
+++ iac/stack/ecs/alb.tf
@@ -0,0 +1,75 @@
+# ALB security group
+resource "aws_security_group" "alb_sg" {
+  name        = "${var.project}-alb-sg"
+  description = "Allow inbound HTTP traffic to ALB"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project}-alb-sg"
+  }
+}
+
+
+# create ALB for private subnet
+resource "aws_lb" "app_alb" {
+  name               = "${var.project}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = data.terraform_remote_state.vpc.outputs.public_subnets
+
+  tags = {
+    Name = "${var.project}-alb"
+  }
+}
+
+# target group
+resource "aws_lb_target_group" "app_tg" {
+  name        = "${var.project}-tg"
+  port        = 5000                       
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
+
+  health_check {
+    path                = "/"
+    port                = "5000"             
+    protocol            = "HTTP"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "${var.project}-tg"
+  }
+}
+
+
+# listener
+resource "aws_lb_listener" "app_listener" {
+  load_balancer_arn = aws_lb.app_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
