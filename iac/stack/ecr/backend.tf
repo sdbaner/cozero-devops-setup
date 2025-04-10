d592d214bff2d76874815204ecd6b94ab70ccef1
Doyel Banerjee <50516377+sdbaner@users.noreply.github.com>
2025-04-10 07:00:22 +0200

    Update backend.tf

--- iac/stack/ecr/backend.tf
+++ iac/stack/ecr/backend.tf
@@ -1,6 +1,20 @@
-backend "s3" {
-    bucket = "cozero-terraform-state-bucket"
-    key    = "shared/ecr/terraform.tfstate"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "cozero-terraform-state-bucket"
+    key            = "shared/ecr/terraform.tfstate"
     dynamodb_table = "terraform-lock-table"
-    region = "eu-central-1"
+    region         = "eu-central-1"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-central-1"
 }
